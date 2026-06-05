66c25690801e9346b7c3deb15fbec13899bd706b
Aaron Aston <astonkid08@gmail.com>
2026-06-04 19:01:40 -0500

    middleware, order routes, MongoDb auto login

---
+++ backend/middleware/authorizeRole.js
@@ -0,0 +1,11 @@
+function authorizeRole(role) {
+    return function (req, res, next) {
+        if (req.user.role !== role) {
+            return res.status(403).json({ message: "Forbidden. Admins only boi" });
+        }
+        next();
+    };
+}
+
+module.exports = authorizeRole;
+
